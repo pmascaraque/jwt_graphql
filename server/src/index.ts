3b81d35342f2d34051e2c7eb97c1b5b18cc0d3a1
Pablo Moreno <ssm.pablo@gmail.com>
2022-09-02 00:30:51 +0200

    graphql & types set up

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,22 @@
+import "reflect-metadata"
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { UserResolver } from "./UserResolvers";
 
-(async ()=> {
+(async () => {
   const app = express();
   app.get("/", (_req, res) => res.send("hello"));
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [UserResolver]
+    })
+  });
+  
+  await apolloServer.start(); 
+  apolloServer.applyMiddleware({ app });
+
   app.listen(4000, () => {
     console.log("express server started");
   });
