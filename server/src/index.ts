b5c1ca0113863cfa217789d7ff389438380eb47e
Pablo Moreno <ssm.pablo@gmail.com>
2022-09-01 21:19:42 +0200

    apollo express running

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,9 @@
-import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import express from 'express';
 
-AppDataSource.initialize().then(async () => {
-
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
-
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
-
-    console.log("Here you can setup and run express / fastify / any other framework.")
-
-}).catch(error => console.log(error))
+(async ()=> {
+  const app = express();
+  app.get("/", (_req, res) => res.send("hello"));
+  app.listen(4000, () => {
+    console.log("express server started");
+  });
+})();
