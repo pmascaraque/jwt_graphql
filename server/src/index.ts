aa283b2fff0eaee86b98c2130c9f2a732ce66135
Pablo Moreno <ssm.pablo@gmail.com>
2022-09-05 01:42:42 +0200

    find user

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,13 +6,36 @@ import { buildSchema } from 'type-graphql';
 import { UserResolver } from "./UserResolvers";
 import { AppDataSource } from "./data-source";
 import cookieParser from "cookie-parser";
+import { verify } from "jsonwebtoken";
+import { User } from "./entity/User";
+import { createAccessToken } from "./auth";
 
 (async () => {
   const app = express();
   app.use(cookieParser());
   app.get("/", (_req, res) => res.send("hello"));
-  app.post("/refresh_token", (req) => {
-    console.log(req.cookies);
+  app.post("/refresh_token", async (req, res) => {
+    const token = req.cookies.jid;
+    if (!token) {
+      return res.send({ ok: false, accessToken: '' })
+    }
+
+    let payload: any = null;
+    try {
+      payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
+    } catch (err) {
+      console.log(err);
+      return res.send({ ok: false, accessToken: '' })
+    }
+  
+    //token is valid, send back token
+    const user = await User.findOne({ id: payload.userId });
+
+    if (!user) {
+      return res.send({ ok: false, accessToken: '' })
+    }
+
+    return res.send({ ok: true, accessToken: createAccessToken(user) })
   })
 
   await AppDataSource.initialize();
